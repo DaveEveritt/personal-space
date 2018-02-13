4faf5180b612bf60131535f35edacf2b7a05acc6
Dave Everitt <deveritt@innotts.co.uk>
2018-02-13 21:45:19 +0000

    JSON data test with fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
 (function(){
-  let data = "hello";
-  console.log(data);
+  // let data = "hello";
+  // console.log(data);
+
+  // test:
+  window.fetch('http://services.swpc.noaa.gov/products/noaa-scales.json')
+    .then(function(response){
+        return response.json();
+    }).then(function(json){
+        return console.log(json);
+    });
 })();
+
+
