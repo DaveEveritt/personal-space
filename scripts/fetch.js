dd9558d2c5d6158c3031d508908d7eed09525d4a
Dave Everitt <deveritt@innotts.co.uk>
2018-02-20 10:43:30 +0000

    basic test data coming in

---
+++ scripts/fetch.js
@@ -0,0 +1,30 @@
+
+"use strict";
+
+(function(){
+
+  let request = new Request('http://services.swpc.noaa.gov/products/noaa-scales.json');
+
+  // let spaceData = [];
+  let spaceData = "";
+
+  fetch(request)
+    .then(function (response) {
+    // console.log(`res: ${response.status}`);
+    return response.json();
+    })
+    .then(function(data) {
+    for(let key in data) {
+      let x = data[key];
+      // spaceData = `${x.R.Scale}, ${x.S.Scale}, ${x.G.Scale}`;
+      // spaceData += x.R.Scale + x.S.Scale + x.G.Scale;
+      spaceData += x.R.Scale + "," + x.S.Scale + "," + x.G.Scale + "\n";
+    }
+    console.log(spaceData);
+    // return(spaceData);
+  });
+
+  // console.log(spaceData);
+
+})();
+// https://github.com/CTEC3905/08-lab-json-ajax/blob/flickr/javascript/scripts.js
--- scripts/script.js
+++ scripts/script.js
@@ -3,24 +3,26 @@
 (function(){
 
   const noaaScales = 'http://services.swpc.noaa.gov/products/noaa-scales.json';
+  
+  let spaceData = {};
 
-  const getData = function(getData) {
-    window.fetch(getData)
+  const getData = function(DataURL) {
+    window.fetch(DataURL)
     .then(function(response){
       return response.json();
     }).then(function(json){
-      console.log(json[0]);
+      // console.log(json[0]);
       // nothing?:
-      return json;
+      spaceData = json;
+      console.log(spaceData[0]);
+      return spaceData;
     });
-    // fails:
-    // return json;
   }
   
   getData(noaaScales);
 
   // undefined, json is not returned:
-  const noaaData = getData(noaaScales);
-  console.log(noaaData);
+  // const noaaData = getData(noaaScales);
+  console.log(spaceData[0]);
 
 })();
