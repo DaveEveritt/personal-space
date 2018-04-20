80d9bd5e3ff3ecd3a5c5f48e95ee9e7c3efd8036
Dave Everitt <deveritt@innotts.co.uk>
2018-04-20 10:09:07 +0100

    tidy up centre panel data display

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -63,7 +63,7 @@
 
   function dataShowKP(kp, au) {
     // console.log(kp);
-    document.getElementById("five").innerHTML = `Kp ${getDataScale(kp)}<br>Solar wind: ${au}AU`;
+    document.getElementById("five").innerHTML = `Kp ${getDataScale(kp)}<br><br>Solar wind: ${au}AU`;
   }
   function getDataScale(kp) {
     // 0-1 quiet, 2-4 unsettled/active, 5 minor storm, 6 larger storm, 7-9 major storm
