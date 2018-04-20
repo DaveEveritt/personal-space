c4f6c04ba51e57572c8749de46d906c91c7ddbb7
Dave Everitt <deveritt@innotts.co.uk>
2018-04-20 15:50:14 +0100

    random string or 60 chars taken from all text

--- scripts/parse-text.js
+++ scripts/parse-text.js
@@ -1,18 +1,30 @@
 (function(){
   // let wordData = document.getElementById("words");
+  const panels = ["four","nine","two","three","seven","eight","one","six"];
 
-  // parses the JSON-formatted string from the file into a JavaScript object
   let obj = JSON.parse(jsonString);
   let wordData = "";
   
   for (key in obj.words) {
-    let text = obj.words[key].text.substring(0, 80);
-    wordData += `${obj.words[key].file}\n${obj.words[key].title}\n${text}\n\n`;
+    let text = obj.words[key].text;
+    // let text = obj.words[key].text.substring(0, 55);
+    wordData += ` ${text}`;
+    // wordData += `${obj.words[key].file}\n\n${obj.words[key].title}\n${text}\n\n`;
   }
-  // console.log(wordData);
-  // demoData.innerHTML = wordData;
+  // console.log(`${wordData}`);
+  
+  let x = 0;
+  let x2 = 60;
+  for (panel in panels) {
+    // 10000 is a guess, need exact number
+    x = parseInt(Math.random()*10000);
+    console.log(x + "\n");
+    document.getElementById(panels[panel]).innerHTML = wordData.substring(x, x + x2);
+    x += x2;
+    x2 += x2;
+  }
 }());
- 
+
 /*
 id 0
 file "text",
@@ -20,4 +32,6 @@ title "text",
 created_date "2018-02-03 14:28:35",
 text "text",
 images "resources/FILENAME.EXT"
+will fit (55chars):
+And you have read my mind again Alice in these prompts!
 */
