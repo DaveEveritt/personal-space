915845614b9ad76fd34c4a662c4b9b32954f176a
Fania Ra <fania@web.de>
2018-12-06 12:11:49 +0000

    change the way start and end words are cut (i.e. only allow full words)

--- scripts/parse-text.js
+++ scripts/parse-text.js
@@ -22,7 +22,7 @@
 
 
   let category = {
-    "low": ["lov", "brill", "wonderful", "sex", "tidy", "excit", "respect", "pleasur", "art", "transcend", "beaut", "gentle", "success", "compassion", "hope", "kind", "nurture", "help", "smil", "beautiful", "comfort", "focus", "desir", "energ", "trust", "proud", "appreciat", "affect", "empath", "brave", "hug", "nourish", "friend", "relax", "like", "nest", "close", "happ", "together", "womb", "truth", "touch", "permiss", "grow", "clear", "birth", "play", "young", "wisdom", "gain", "surprise", "humour"],
+    "low": ["lov", "brill", "wonderful", "sex", "tidy",     "excit", "respect", "pleasur", "art", "transcend", "beaut", "gentle", "success", "compassion", "hope", "kind", "nurture", "help", "smil", "beautiful", "comfort", "focus", "desir", "energ", "trust", "proud", "appreciat", "affect", "empath", "brave", "hug", "nourish", "friend", "relax", "like", "nest", "close", "happ", "together", "womb", "truth", "touch", "permiss", "grow", "clear", "birth", "play", "young", "wisdom", "gain", "surprise", "humour"],
 
     "mid": ["realis", "activ", "danc", "doubt", "unusual", "quirky", "torn", "action", "secret", "endur", "mov", "inabil", "entangl", "sacrif", "scatt", "confus", "limit", "disinterest", "ignor", "hid", "risk", "late", "odd", "obstacle", "hindrance", "lonely", "sorrow", "sudden", "strange", "confus", "love/hate", "mother", "father", "potential", "body", "transform", "synchron", "surprise", "alert"],
 
@@ -133,14 +133,39 @@
       // DONT FORGET TO UPDATE THESE !!!
       if (currentKP == "low") {
         x = parseInt(Math.random()*(819679-120)); // length of total text - 120;
+        while(x > 0 && wordDataLow[x-1] !== " ") {
+          // console.log("not a space before:", x, wordDataLow[x-1]);
+          x--;  // go to start of a word at beginning
+        }
+        while(!strayChars.includes(wordDataLow[x+x2])) {
+          // console.log("not a space at end:", x2, wordDataLow[x+x2]);
+          x2--;  // go to end of a word at end
+        }
         textChunk = wordDataLow.substring(x, x + x2);
+        // console.log("text: ", textChunk);
       }
       if (currentKP == "mid") {
         x = parseInt(Math.random()*(26038-120)); // length of total text - 120;
+        while(x > 0 && wordDataMid[x-1] !== " ") {
+          // console.log("not a space before:", x, wordDataLow[x-1]);
+          x--;  // go to start of a word at beginning
+        }
+        while(!strayChars.includes(wordDataMid[x+x2])) {
+          // console.log("not a space at end:", x2, wordDataLow[x+x2]);
+          x2--;  // go to end of a word at end
+        }
         textChunk = wordDataMid.substring(x, x + x2);
       }
       if (currentKP == "high") {
         x = parseInt(Math.random()*(71482-120)); // length of total text - 120;
+        while(x > 0 && wordDataHigh[x-1] !== " ") {
+          // console.log("not a space before:", x, wordDataLow[x-1]);
+          x--;  // go to start of a word at beginning
+        }
+        while(!strayChars.includes(wordDataHigh[x+x2])) {
+          // console.log("not a space at end:", x2, wordDataLow[x+x2]);
+          x2--;  // go to end of a word at end
+        }
         textChunk = wordDataHigh.substring(x, x + x2);
       }
 
